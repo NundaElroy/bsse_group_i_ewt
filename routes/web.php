7dacabdce52c60369725d058069dd329cfebc9d3
Alvin170803 <ainebyonaalvinpolice72@gmail.com>
2025-04-07 15:12:59 +0300

    (fix)   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use App\Http\Controllers\ContactController;
 Route::get('/home', function () {
     return view('home');
 });
-Route::get('/', function () {
-    return view('home');
-});
+
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
